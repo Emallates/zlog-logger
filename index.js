388d0452c22c7b6c59d51e4b8f4e6146478f0a5c
zeeshan <zeeshan_asyed@hotmail.com>
2016-07-19 11:18:51 +0400

    shifted zglog-logger

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
+module.exports = function(opts){ return opts.request != undefined && opts.response != undefined && opts.tpr != undefined ? require('./lib')(opts).log() : require('./lib')(opts).logger(); };
 // module.exports = function(opts){
 // 	// if mode == 'local' return logger
 // 	//  if mode == 'central' return logger 
 //    return require('./lib')(opts);
 // }
-module.exports = function(opts){
-   return opts.request != undefined && opts.response != undefined && opts.tpr != undefined ? require('./lib')(opts).log() : require('./lib')(opts).logger();
-
-};
